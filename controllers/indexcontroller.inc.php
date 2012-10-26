a0e14f5e70a9fdaaabdf8a2c8f0c6900fb50c8b1
Mikael Jacobson <mikael@mludd.se>
2012-10-27 00:52:31 +0200

    Added a set of default controllers

---
+++ controllers/indexcontroller.inc.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ * Contains IndexController class
+ * @package Default
+ * @license http://s.mludd.se/COPYING GNU General Public License
+ */
+
+/*
+ * This file is part of Mludd's MVC Framework (MMVC).
+ *
+ * Copyright (C) 2012 Mikael Jacobson <mikael@mludd.se>
+ *
+ * MMVC is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * MMVC is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with MMVC.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+require_once(dirname(__FILE__)."/controller.inc.php");
+/**
+ * Controller for index page
+ * @author Mikael Jacobson <mikael@mludd.se>
+ * @copyright Copyright (c) 2012 Mikael Jacobson
+ */
+class IndexController extends Controller {
+	/**
+	 * Performs page rendering logic
+	 */
+	protected function process() {
+		$this->_templateFile = 'index.tpl';
+	}
+}
+?>
