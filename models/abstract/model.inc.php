46bb4909444181dc243e6cc0b93ae8236978c981
mludd <mikael@mludd.se>
2014-03-29 13:50:55 +0100

    Updated Copyright notice

--- models/abstract/model.inc.php
+++ models/abstract/model.inc.php
@@ -8,7 +8,7 @@
 /*
  * This file is part of The Simple PHP MVC Framework (siphMVC).
  *
- * Copyright (C) 2012 Mikael Jacobson <mikael@mludd.se>
+ * Copyright (C) 2014 Mikael Jacobson <mikael@mludd.se>
  *
  * siphMVC is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
